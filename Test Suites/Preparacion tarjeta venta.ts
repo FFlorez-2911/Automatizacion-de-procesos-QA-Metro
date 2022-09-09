9ff626c64aa85b7dc8a933de5e0241857c900c40
feflorez <feflorez@UENOP029.metrodemedellin.gov.co>
2022-09-09 09:52:53 -0500

    P

---
+++ Test Suites/Preparacion tarjeta venta.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Preparacion tarjeta venta</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>72415953-bef4-4fae-b680-b7ca6849584f</testSuiteGuid>
+   <testCaseLink>
+      <guid>e65d3533-79b3-40d9-83c2-5ab92fbf5b04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Preparacion tarjeta venta</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
